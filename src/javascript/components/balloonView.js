2c5974a2f65f63ec6c90d8361c3eccbddcb0a07b
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-06-04 14:22:42 +0300

    Implement balloonView

--- src/javascript/components/balloonView.js
+++ src/javascript/components/balloonView.js
@@ -1,3 +1,23 @@
 import createElement from "../helpers/domHelper";
 
-export default function createBalloon() {}
+function createBalloon(width, height, horizontalOffset) {
+  const balloon = createElement({ tagName: "div", className: "balloon" });
+  balloon.style.width = width;
+  balloon.style.height = height;
+  balloon.style.left = horizontalOffset;
+
+  return balloon;
+}
+
+export default function createBalloons(viewportWidth, viewportHeight, amount) {
+  const balloonWidth = Math.round(viewportWidth / amount);
+  const balloonHeight = Math.round(viewportHeight / 4);
+
+  let currentOffset = 0;
+  const balloons = [];
+  for (let i = 0; i < amount; i++) {
+    balloons.push(createBalloon(balloonWidth, balloonHeight, currentOffset));
+    currentOffset += balloonWidth;
+  }
+  return balloons;
+}
