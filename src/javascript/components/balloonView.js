2882e59f1bbd9ed649236510a70bb1af151e55d6
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-06-04 17:27:39 +0300

    Desparate commit

--- src/javascript/components/balloonView.js
+++ src/javascript/components/balloonView.js
@@ -1,23 +1,24 @@
-import createElement from "../helpers/domHelper";
+import createElement from "../helpers/domHelper.js";
 
 function createBalloon(width, height, horizontalOffset) {
   const balloon = createElement({ tagName: "div", className: "balloon" });
-  balloon.style.width = width;
-  balloon.style.height = height;
-  balloon.style.left = horizontalOffset;
+  balloon.style.width = width + "px";
+  balloon.style.height = height + "px";
+  balloon.style.left = horizontalOffset + "px";
 
   return balloon;
 }
 
 export default function createBalloons(viewportWidth, viewportHeight, amount) {
-  const balloonWidth = Math.round(viewportWidth / amount);
-  const balloonHeight = Math.round(viewportHeight / 4);
+  const balloonWidth = Math.min(200, Math.round(viewportWidth / amount));
+  const balloonHeight = Math.min(300, Math.round(viewportHeight / 5));
 
-  let currentOffset = 0;
+  let currentOffset = balloonWidth;
   const balloons = [];
   for (let i = 0; i < amount; i++) {
-    balloons.push(createBalloon(balloonWidth, balloonHeight, currentOffset));
-    currentOffset += balloonWidth;
+    balloons.push(
+      createBalloon(balloonWidth, balloonHeight, currentOffset * i)
+    );
   }
   return balloons;
 }
